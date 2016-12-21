90fbb28014af7f7f0d2263db1155607e043f8f53
Jonathan Cedrim De Souza <jonathan.cedrim.souza@gmail.com>
2016-12-21 12:12:47 -0200

    fix

---
+++ Gui-OO/src/aplication/TesteRectangle.java
@@ -0,0 +1,39 @@
+package aplication;
+
+import domain.Rectangle;
+import domain.Point;
+public class TesteRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Rectangle rectangle = new Rectangle();
+		Point  ponto = new Point();
+		
+		
+		rectangle.witdh  = 14.6;
+		rectangle.height = 8.5;
+		
+		ponto.pontoY = 20;
+		ponto.pontoX = 20;
+		
+		//Error  de Null
+		String resultEstaCondido = rectangle.EstaContido(rectangle);
+		
+		String result = "Area = " + rectangle.area();
+		
+		String resultAmpliar = "Ampliar = " +rectangle.ampliar(10);
+		
+		String resultReduzir = "Reduzir = "+rectangle.reduzir(5);
+		
+		System.out.println("O reslutado da area do retangulo eh "+result);
+		System.out.println("O retangulo ampliado eh = "+resultAmpliar);
+		System.out.println("O retangulo reduzido eh = "+resultReduzir);
+	        
+                System.out.println("Esta contido = "+resultEstaCondido);
+	
+		
+	
+	}
+
+}
