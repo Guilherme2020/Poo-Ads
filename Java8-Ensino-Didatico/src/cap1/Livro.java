e6f1a930aabafbae9f2ee5312c58054c8555a65f
guilherme2020 <guilhermerodrigues123477@gmail.com>
2016-12-23 11:43:00 -0200

    primeiro capitulo

---
+++ Java8-Ensino-Didatico/src/cap1/Livro.java
@@ -0,0 +1,13 @@
+package cap1;
+
+public class Livro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ensino didatico Java 8");
+		System.out.println("Uma maneira fácil e gostosa de aprender");
+	}
+
+}
+
+
