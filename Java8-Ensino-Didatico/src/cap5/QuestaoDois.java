ddedc42f257216eac5c0f166997601c14452211a
guilherme2020 <guilhermerodrigues123477@gmail.com>
2017-01-05 15:51:18 -0200

    Questao Tres

--- Java8-Ensino-Didatico/src/cap5/QuestaoDois.java
+++ Java8-Ensino-Didatico/src/cap5/QuestaoDois.java
@@ -7,7 +7,14 @@ import javax.swing.JOptionPane;
 public class QuestaoDois {
 
 	public static void main(String[] args) {
-	
+
+		String[] meses = {"Janeiro","Fevereiro","Março",
+				"Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+		
+
+		int numeroMes = (int) (Math.random()*12);
+		
+		JOptionPane.showMessageDialog(null, "O mes sorteado foi : "+meses[numeroMes]);
 	}
 
 }
