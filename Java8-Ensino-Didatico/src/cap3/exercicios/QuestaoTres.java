e7f636ddde9b3c8be332504e9e7126ab7e28997b
guilherme2020 <guilhermerodrigues123477@gmail.com>
2017-01-19 11:03:26 -0200

    ! book

--- Java8-Ensino-Didatico/src/cap3/exercicios/QuestaoTres.java
+++ Java8-Ensino-Didatico/src/cap3/exercicios/QuestaoTres.java
@@ -5,30 +5,53 @@ import javax.swing.JOptionPane;
 public class QuestaoTres {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int contador = 0;
-		final String loginOriginal = "java8";
-		final String senhaOriginal = "java8";
-		
-		while(contador < 3 ){
-			
-			String login = JOptionPane.showInputDialog("Insira o login");
-			String senha = JOptionPane.showInputDialog("Insira a Senha");
-			
-			if(login.equals(loginOriginal) && senha.equals(senhaOriginal)){
-				JOptionPane.showMessageDialog(null, "SEja bem vindo");
-				break;
-			}else{
-				contador++;
-				if(contador == 3){
-					JOptionPane.showMessageDialog(null,"Suas chances acabaram");
+//		// TODO Auto-generated method stub
+//		int contador = 0;
+//		final String loginOriginal = "java8";
+//		final String senhaOriginal = "java8";
+//		
+//		while(contador < 3 ){
+//			
+//			String login = JOptionPane.showInputDialog("Insira o login");
+//			String senha = JOptionPane.showInputDialog("Insira a Senha");
+//			
+//			if(login.equals(loginOriginal) && senha.equals(senhaOriginal)){
+//				JOptionPane.showMessageDialog(null, "SEja bem vindo");
+//				break;
+//			}else{
+//				contador++;
+//				if(contador == 3){
+//					JOptionPane.showMessageDialog(null,"Suas chances acabaram");
+//				}else{
+//					JOptionPane.showMessageDialog(null, "Falha no logiin \n "+"Voce tem mais "+ (3 - contador) +"Tentativa!");
+//
+//				}
+//			}
+//			
+//		}
+	
+		String login = "java8";
+		String senha = "java8";
+		int cont = 0;
+		while(login == "java8"){
+			if(cont < 3){
+				String logiIn = JOptionPane.showInputDialog("Login: ");
+				String senhaIn = JOptionPane.showInputDialog("Senha");
+				if(login.equals(logiIn)&& senha.equals(senhaIn)){
+					JOptionPane.showMessageDialog(null,"Acesso permitido");
+					break;
 				}else{
-					JOptionPane.showMessageDialog(null, "Falha no logiin \n "+"Voce tem mais "+ (3 - contador) +"Tentativa!");
-
+					cont++;
+					if(cont == 3){
+						JOptionPane.showMessageDialog(null,"Acesso cancelado tentativas encerradas");
+						break;
+					}if(!login.equals(logiIn) && !senha.equals(senhaIn)){
+						JOptionPane.showMessageDialog(null,"tente novamente"+cont);
+					}
 				}
+				
 			}
-			
 		}
+	
 	}
-
 }
--- Java8-Ensino-Didatico/src/cap5/QuestaoDois.java
+++ Java8-Ensino-Didatico/src/cap5/QuestaoDois.java
@@ -1,25 +1,40 @@
 package cap5;
 
-import java.lang.Math;
+import java.util.Scanner;
 
-import javax.swing.JOptionPane;
 
 public class QuestaoDois {
 
+	
 	public static void main(String[] args) {
 
-		String[] meses = {"Janeiro","Fevereiro","Março",
-				"Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
-		
+//		String[] meses = {"Janeiro","Fevereiro","Março",
+//				"Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+//		
+//
+//		int numeroMes = (int) (Math.random()*12);
+//		
+//		
+//		
+//		JOptionPane.showMessageDialog(null, "O mes sorteado foi : "+meses[numeroMes]);
+//	
+//	
+//	
+		  	float x=0;
+	        float y=0;
+	        
+	        System.out.println("x");
+	        
+	        x = new Scanner(System.in).nextFloat();
+	        
+	        System.out.println("y");
+	        y = new Scanner(System.in).nextFloat();
+	        float distancia = x / y;
 
-		int numeroMes = (int) (Math.random()*12);
+	        System.out.println("%.3f = km/l\n"+ distancia);
+
 		
 		
-		
-		JOptionPane.showMessageDialog(null, "O mes sorteado foi : "+meses[numeroMes]);
-	
-	
-	
 	}
 
 }
