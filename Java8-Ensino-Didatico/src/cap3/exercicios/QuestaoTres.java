de4a4955796f3df538c356f8c5c4ee74344a9717
guilherme2020 <guilhermerodrigues123477@gmail.com>
2017-01-19 11:09:22 -0200

    ! bookJa

--- Java8-Ensino-Didatico/src/cap3/exercicios/QuestaoTres.java
+++ Java8-Ensino-Didatico/src/cap3/exercicios/QuestaoTres.java
@@ -6,29 +6,7 @@ public class QuestaoTres {
 
 	public static void main(String[] args) {
 //		// TODO Auto-generated method stub
-//		int contador = 0;
-//		final String loginOriginal = "java8";
-//		final String senhaOriginal = "java8";
-//		
-//		while(contador < 3 ){
-//			
-//			String login = JOptionPane.showInputDialog("Insira o login");
-//			String senha = JOptionPane.showInputDialog("Insira a Senha");
-//			
-//			if(login.equals(loginOriginal) && senha.equals(senhaOriginal)){
-//				JOptionPane.showMessageDialog(null, "SEja bem vindo");
-//				break;
-//			}else{
-//				contador++;
-//				if(contador == 3){
-//					JOptionPane.showMessageDialog(null,"Suas chances acabaram");
-//				}else{
-//					JOptionPane.showMessageDialog(null, "Falha no logiin \n "+"Voce tem mais "+ (3 - contador) +"Tentativa!");
-//
-//				}
-//			}
-//			
-//		}
+
 	
 		String login = "java8";
 		String senha = "java8";
