7ae99e23077fb268a98adf79de3fcb2cccc7e1eb
guilherme2020 <guilhermerodrigues123477@gmail.com>
2017-01-24 22:27:05 -0200

    mes cap5 !=

--- Java8-Ensino-Didatico/src/cap5/QuestaoDois.java
+++ Java8-Ensino-Didatico/src/cap5/QuestaoDois.java
@@ -7,16 +7,12 @@ public class QuestaoDois {
 	
 	public static void main(String[] args) {
 
-		String[] meses = {"Janeiro","Fevereiro","Março",
-				"Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
-		
+		String[] meses = {"1","2","3","4","5","6","7","8","9","10","11","12"};
 
-		int numeroMes = (int) (Math.random()*12);
-		
-		
-		
-		System.out.println("O mes sorteado foi : "+meses[numeroMes]);
-	
+	      int numero = (int) (Math.random()*12);
+
+	      System.out.println("O mes do ano sorteado foi o mês: "+meses[numero]);
+
 
 				
 		
