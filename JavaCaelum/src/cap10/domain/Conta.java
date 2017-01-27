8e0308750d7bf7a73273a90af91632e9b2554fab
guilherme2020 <guilhermerodrigues123477@gmail.com>
2017-01-27 02:20:29 -0200

    Exceptions

---
+++ JavaCaelum/src/cap10/domain/Conta.java
@@ -0,0 +1,31 @@
+package cap10.domain;
+
+public  abstract class Conta {
+
+	protected double saldo;
+	
+	
+	public double getSaldo() {
+		return saldo;
+		
+	}
+	
+	
+	public void deposita(double valor){
+		if (valor <0) {
+			throw new IllegalArgumentException();
+		}else{
+			this.saldo += valor;
+
+		}
+	}
+	
+	public void saca(double valor){
+		this.saldo -= valor;
+	}
+	
+	public  abstract void atualiza(double taxa);
+
+
+	
+}
