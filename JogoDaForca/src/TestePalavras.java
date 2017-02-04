e766a329fb34605703628746c74c9fe805ad6e49
guilherme2020 <guilhermerodrigues123477@gmail.com>
2017-02-04 19:04:01 -0200

    array list

--- JogoDaForca/src/TestePalavras.java
+++ JogoDaForca/src/TestePalavras.java
@@ -1,4 +1,6 @@
 import java.util.*;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 public class TestePalavras {
 
 	
@@ -7,29 +9,39 @@ public class TestePalavras {
 	
 	public static void main(String[] args) {
 		
-		String[] vetorDePalavras = {"Gordo","erico","aba","asa"};
-		String lerPrimeiraPalavra,lerSegundaPalavra;
+//		String[] vetorDePalavras = {"Gordo","erico","aba","asa"};
+//		String lerPrimeiraPalavra,lerSegundaPalavra;
+//		
+//		int sorteioDoVector = (int)(Math.random()* vetorDePalavras.length);
+//		
+//		System.out.print(sorteioDoVector+"\n");
+//		
+//		System.out.print(vetorDePalavras[sorteioDoVector]);
+//		
 		
-		int sorteioDoVector = (int)(Math.random()* vetorDePalavras.length);
-		
-		System.out.print(sorteioDoVector+"\n");
-		
-		System.out.print(vetorDePalavras[sorteioDoVector]);
+		List<String> palavras = Arrays.asList("oi","gordo");
 		
 		
-		
-		
-		
-		System.out.println("\nInsira a primeira Palavra");
-		lerPrimeiraPalavra = new Scanner(System.in).next();
+		String palavraEscolhida = (int)palavras[Math.random()*2];
+//		
+//		System.out.println("\nInsira a primeira Palavra");
+//		lerPrimeiraPalavra = new Scanner(System.in).next();
+//		
+//		
 		
 		
 		System.out.println("Insira a segunda Palavra");
 		
 		lerSegundaPalavra = new Scanner(System.in).next();
 		
+		
+		
+		
+		
+		
+		
 		for (int i = 0; i < vetorDePalavras.length; i++) {
-			if (lerPrimeiraPalavra == vetorDePalavras[sorteioDoVector]) {
+			if (lerPrimeiraPalavra == vetorDePalavras[sorte]) {
 				
 			}
 		}
